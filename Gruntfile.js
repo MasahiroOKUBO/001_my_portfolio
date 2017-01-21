f83ff0d50b26b869ab00bd69a4a1be662073259e
MasahiroOKUBO <okubo.masahiro@okubo-tech.com>
2017-01-21 12:24:18 +0900

    v1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,9 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    //no need to grunt.loadNpmTasks('task_name');
+    require('load-grunt-tasks')(grunt);
+
+    // use config file, if need.
+    var config = grunt.file.readYAML('Gruntconfig.yml');
 
     grunt.initConfig({
         responsive_images: {
@@ -6,22 +11,76 @@ module.exports = function(grunt) {
                 options: {
                     engine: 'im',
                     sizes: [{
-                        width: 800,
-                        suffix: '_large_1x',
+                        width: 1200,
+                        suffix: '_large',
+                        quality: 50
+                    },{
+                        width: 600,
+                        suffix: '_medium',
+                        quality: 50
+                    },{
+                        width: 400,
+                        suffix: '_small',
                         quality: 50
                     }]
                 },
                 files: [{
                     expand: true,
                     src: ['*.{gif,jpg,png}'],
-                    cwd: 'src/images_src/',
-                    dest: 'src/images/'
+                    cwd: '01_src/img_src/',
+                    dest: '01_src/img/'
                 }]
             }
         },
+
+        cssmin : {
+            mystyles : {
+                src : ['01_src/css/mystyles.css'],
+                dest : '01_src/css/mystyles.min.css'
+            },
+        },
+
+        concat: {
+            dist: {
+                src: '01_src/js_src/*js',
+                dest: '01_src/js/app.js'
+            }
+        },
+
+        jshint: {
+            options:{
+                "eqeqeq": true
+            },
+            all:[
+                'Gruntfile.js',
+                config.src_dir_js+'*.js'
+            ]
+        },
+
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: config.src_dir, src: ['*.html'], dest: config.dst_dir},
+                    {expand: true, cwd: config.src_dir, src: ['css/*.min.css'], dest: config.dst_dir},
+                    {expand: true, cwd: config.src_dir, src: ['js/*.min.js'], dest: config.dst_dir},
+                    {expand: true, cwd: config.src_dir, src: ['img/*'], dest: config.dst_dir},
+                    {expand: true, cwd: config.src_dir, src: ['fonts/*'], dest: config.dst_dir},
+                ],
+            },
+        },
+
+        watch: {
+
+        }
+
     });
 
-    grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.registerTask('default', ['responsive_images']);
+    grunt.registerTask('default', [
+        'responsive_images',
+        'cssmin',
+    ]);
 
+    grunt.registerTask('make_distribution', [
+        'copy',
+    ]);
 };
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 001_my_portfolio
+
+
+01_src is my working directory. only use for dev. no need to run. so you can delete it.
+
+
+02_dst is distribution directory.
+
+
+
